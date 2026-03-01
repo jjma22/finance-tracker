8db8d5697853373013f84fef671323a949bc4384
Joseph Adams <172708@student.stbrn.ac.uk>
2026-03-01 21:27:10 +0000

    ref: Update code to use handle func

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -20,36 +20,11 @@ func FinanceNewServer(l *log.Logger) *financeServer{
 }
 
 func (f*financeServer) ServeHTTP(rw http.ResponseWriter, r*http.Request) {
-	// refactor so cpature is case insensitive
-	if r.Method == http.MethodGet && r.URL.Path == "/monthlybudget"{
-		f.GetBudget(rw, r)
-		return
 
-	}
 
-	if r.Method == http.MethodPut && r.URL.Path == "/monthlybudget"{
-		f.UpdateBudget(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodGet && r.URL.Path == "/expense"{
-		f.GetExpenses(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodGet && r.URL.Path == "/expense/total"{
-		f.GetTotalExpense(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPost && r.URL.Path == "/expense"{
-		f.AddExpense(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPut && strings.HasPrefix(r.URL.Path, "/expense/update/") {
-		f.UpdateExpense(rw, r)
-	}
+	// if r.Method == http.MethodPut && strings.HasPrefix(r.URL.Path, "/expense/update/") {
+	// 	f.UpdateExpense(rw, r)
+	// }
 
 	if r.Method == http.MethodDelete && strings.HasPrefix(r.URL.Path, "/expense/delete/") {
 		f.DeleteExpense(rw, r)
@@ -136,26 +111,26 @@ func (f*financeServer) UpdateExpense( rw http.ResponseWriter, r*http.Request) {
 	f.l.Println("Updating expense")
 	
 	// Get ID from path
-	e := strings.Split(r.URL.Path,`/`)
-	f.l.Println(len(e))
-	if (len(e) != 4) {
-		f.l.Println("Bad request, invalid path")
-		http.Error(rw, "Invalid path", http.StatusBadRequest)
-	}
-	id, err := strconv.Atoi(e[3])
-	if err != nil {
-		f.l.Println("Invalid request sent to UpdateExpense")
-		http.Error(rw, "Bad request", http.StatusBadRequest)
-	}
+	// e := strings.Split(r.URL.Path,`/`)
+	// f.l.Println(len(e))
+	// if (len(e) != 4) {
+	// 	f.l.Println("Bad request, invalid path")
+	// 	http.Error(rw, "Invalid path", http.StatusBadRequest)
+	// }
+	// id, err := strconv.Atoi(e[3])
+	// if err != nil {
+	// 	f.l.Println("Invalid request sent to UpdateExpense")
+	// 	http.Error(rw, "Bad request", http.StatusBadRequest)
+	// }
 
 	var exp data.Expense
-	err = json.NewDecoder(r.Body).Decode(&exp)
+	err := json.NewDecoder(r.Body).Decode(&exp)
 	if err != nil {
 		f.l.Println("Error unmarshalling request")
 		http.Error(rw, "Unable to unmarshall request", http.StatusInternalServerError)
 	}
 
-	exp.ID = id
+	exp.ID, _ = strconv.Atoi(r.PathValue("id"))
 	err = data.UpdateExpense(&exp)
 	if err != nil {
 		f.l.Println(err)
