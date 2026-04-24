a2218d9c1386b978af2e6b170ef11e1949faaa87
jjma22 <joe.adams123@hotmail.co.uk>
2026-04-24 08:25:49 +0100

    fix: remove used code

--- internal/data/expenses.go
+++ internal/data/expenses.go
@@ -1,9 +1,7 @@
 package data
 
 import (
-	"errors"
 	"regexp"
-	"slices"
 	"time"
 
 	"github.com/go-playground/validator"
@@ -20,12 +18,6 @@ type Expense struct {
 }
 
 type Expenses []*Expense
-
-// Simply func but easier to rewrite
-// once database is added
-func GetExpenses() Expenses {
-	return MonthlyExpenses
-}
 
 func (e *Expense) Validate() error {
 	validate := validator.New()
@@ -50,70 +42,3 @@ func validateSKU(fl validator.FieldLevel) bool {
 }
 
 var MonthlyExpenses = Expenses{}
-
-// Module to search if field value already exisits
-// This will need rewriting when applicaiton uses database
-func (Expense) SearchFields(f any, v any) (bool, error) {
-	// Create array of values from specific field
-	var currentValues []any
-	for _, expense := range MonthlyExpenses {
-		switch f {
-		case "Name":
-			currentValues = append(currentValues, expense.Name)
-		case "ID":
-			currentValues = append(currentValues, expense.ID)
-		default:
-			return false, errors.New("Invalid field")
-
-		}
-	}
-
-	// return true if slice contains values
-	if slices.Contains(currentValues, v) {
-		return true, nil
-	} else {
-		return false, nil
-	}
-}
-
-// No longer used sinced migrated to db
-// func UpdateExpense(e *Expense) error {
-// 	v, err := e.SearchFields("ID", e.ID)
-// 	if err != nil {
-// 		return errors.New("Error searching for ID")
-// 	}
-// 	if v == false {
-// 		return errors.New("Invalid ID")
-// 	}
-// 	fmt.Println("Expense found, updating")
-// 	fmt.Println(e.Price)
-// 	MonthlyExpenses[(e.ID - 1)].Price = e.Price
-// 	//MonthlyExpenses[(e.ID - 1)].LastUpdate = time.Now().Truncate(time.Second).Format("2006-01-02 15:04:05")
-// 	return nil
-
-// }
-
-func DeleteExpense(i int) error {
-	var e Expense
-	e.ID = i
-	v, err := e.SearchFields("ID", i)
-	if err != nil {
-		return errors.New("Error searching for ID")
-	}
-	if v == false {
-		return errors.New("ID does not exist")
-	}
-
-	MonthlyExpenses = slices.Delete(MonthlyExpenses, (i - 1), i)
-	return nil
-
-}
-
-// No longer needed now db implemented
-// func GetTotal() (float32, error) {
-// 	var t float32
-// 	for _, e := range MonthlyExpenses {
-// 		t += e.Price
-// 	}
-// 	return t, nil
-// }
